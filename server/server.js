fbdd83fc0934a10a1f5a304ad6ad20f1f399d596
pathnirvana <pathnirvana@gmail.com>
2020-06-25 13:24:53 +0700

    add titles and static serving to server.js

--- server/server.js
+++ server/server.js
@@ -9,12 +9,17 @@
  */
 const path = require('path')
 const fs = require('fs')
+const getTitle = require('./title-creator.js')
 const fastify = require('fastify')({
-    logger: true
+    logger: false
 })
 fastify.register(require('fastify-cors'), { 
     origin: true, 
 })
+
+const titleStr = '<title>Buddha Jayanthi Tripitaka</title>' // replace this
+const ogTitleStr = '<meta property=og:title content="බුද්ධ ජයන්ති ත්‍රිපිටකය">'
+
 const SqliteDB = require('./sql-query.js')
 const ftsDb = new SqliteDB(path.join(__dirname, 'fts.db'), false)
 const dictDb = new SqliteDB(path.join(__dirname, 'dict-all.db'), false)
@@ -35,13 +40,19 @@ fastify.post('/tipitaka-query/dict', async (request, reply) => { // hit dict db
 
 fastify.register(require('fastify-static'), {
     root: path.join(__dirname, '../dist'),
-    //prefix: '/public/', // optional: default '/'
 })
 
 // needed to serve index.html when url is not found (same as nginx try_files)
-fastify.setNotFoundHandler(async (request, reply) => { 
-    const bufferIndexHtml = fs.readFileSync(path.join(__dirname, '../dist/index.html'))
-    reply.type('text/html').code(200).send(bufferIndexHtml)
+fastify.setNotFoundHandler(async (request, reply) => {
+    let indexHtml = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf-8')
+    // fill in a title for Facebook links
+    const newTitle = getTitle(request.raw.url)
+    if (newTitle) {
+        console.log(`${request.raw.url} => ${newTitle}`)
+        indexHtml = indexHtml.replace(titleStr, `<title>${newTitle}</title>`)
+        indexHtml = indexHtml.replace(ogTitleStr, `<meta property=og:title content="${newTitle}">`)
+    }
+    reply.type('text/html').code(200).send(indexHtml)
 })
 
 // in dev environment 0.0.0.0 listens to requests from all LAN devices
