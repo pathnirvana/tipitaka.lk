1c46c06e3b9af1db0b797c1ef8aa7b1ab973983f
pathnirvana <pathnirvana@gmail.com>
2020-09-05 01:41:56 +0700

    adding atta-an-4

--- dev/atta-headings/match-headings-atta.js
+++ dev/atta-headings/match-headings-atta.js
@@ -12,8 +12,8 @@ const vkb = require('vkbeautify')
 const path = require('path')
 
 // following files were not processed - dn, mn-1 - headings were already good or copied manually
-const filename = 'atta-an-3'
-const isSimpleCopy = true // simply copy the headings without any modification
+const filename = 'atta-an-4'
+const isSimpleCopy = false // simply copy the headings without any modification
 const tree = JSON.parse(fs.readFileSync(__dirname + '/../../public/static/data/tree.json', { encoding: 'utf-8' }))
 const keysToProcess = Object.keys(tree).filter(k => (tree[k][5] == filename && tree[k][2] <= 4))
 const data = JSON.parse(fs.readFileSync(`${__dirname}/../../public/static/text/${filename}.json`, { encoding: 'utf-8' }))
@@ -42,19 +42,18 @@ keysToProcess.forEach(akey => {
         console.error(`Malformed atta heading ${attaH}`)
         return
     }
-    //if (attaRes[2].trim() && mulaRes[2].search(attaRes[2].substr(0, 3)) == -1) {
-    //    diff.push(`name mismatch,${key},${level},${attaH},${mulaRes[2]}`)
-    //}
+    if (attaRes[2].trim() && mulaRes[2].search(attaRes[2].substr(0, 3)) == -1) {
+        diff.push(`name mismatch,${key},${level},${attaH},${mulaRes[2]}`)
+    }
 
-    // attaDigit used in build-tree as keyOffset
-    const attaDigit = attaRes[1], isRange = attaRes[1].indexOf('-') >= 0
+    const attaDigit = attaRes[1].trim() ? (attaRes[1].trim() + ' ') : '', isRange = attaRes[1].indexOf('-') >= 0
     if (isSimpleCopy) { if (level > 1) pEnt.text = tree[key][0] }
     else if (mulaRes[2].trim()) { // only if mula name is non empty
         if (level == 1 && !/සුත්(තං|තානි)$/.test(mulaRes[2])) { 
             diff.push(`pali not sutta,${key},${level},${attaH},${mulaRes[2]}`)
             return // do not replace
         }
-        const newPaliH = attaDigit + getPaliName(isRange, mulaRes[2], level)
+        const newPaliH = level > 1 ? tree[key][0] : (attaDigit + getPaliVanna(isRange, mulaRes[2], level))
         if (pEnt.text != newPaliH)
             replaceMap.push(`${key}\t\tpali\t\t${pEnt.text}\t\t${newPaliH}`)
         pEnt.text = newPaliH
@@ -68,7 +67,7 @@ keysToProcess.forEach(akey => {
             diff.push(`sinh not suttra,${key},${level},${tree[akey][1]},${msinhRes[2]}`)
             return
         }
-        const newSinhH = attaDigit + getSinhName(isRange, msinhRes[2], level)
+        const newSinhH = level > 1 ? tree[key][1] : (attaDigit + getSinhVanna(isRange, msinhRes[2], level))
         sEnt.text = newSinhH
         //console.log(newSinhH)
     }
@@ -80,8 +79,7 @@ if (!dryRun) {
 fs.writeFileSync(path.join(__dirname, 'diff', `${filename}-replace.txt`), replaceMap.join('\n'), {encoding: 'utf-8'})
 fs.writeFileSync(path.join(__dirname, 'diff', `${filename}-diff.txt`), diff.join('\n'), {encoding: 'utf-8'})
 
-function getPaliName(isRange, mulaName, level) { // vannana or suttadivannana for level 1
-    if (level > 1) return mulaName
+function getPaliVanna(isRange, mulaName) { // vannana or suttadivannana for level 1
     let newHeading = mulaName + 'වණ්ණනා' // -සුත්තානිවණ්ණනා
     if (mulaName.endsWith('සුත්තං')) {
         const ending = isRange ? 'සුත්තාදිවණ්ණනා' : 'සුත්තවණ්ණනා'
@@ -90,8 +88,7 @@ function getPaliName(isRange, mulaName, level) { // vannana or suttadivannana fo
     return newHeading.replace(/ආදිසුත්තවණ්ණනා|ආදිසුත්තාදිවණ්ණනා/, 'සුත්තාදිවණ්ණනා')
 }
 
-function getSinhName(isRange, mulaName, level) {
-    if (level > 1) return mulaName
+function getSinhVanna(isRange, mulaName) {
     let newHeading = mulaName + ' වර්ණනා' // -සූත්‍ර වර්ණනා
     if (mulaName.endsWith('සූත්‍රය')) {
         const ending = isRange ? 'ආදි සූත්‍ර වර්ණනා' : 'සූත්‍ර වර්ණනාව'
