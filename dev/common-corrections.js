fc86b3cbedcd85aae6fef1401eee237559f176ac
pathnirvana <pathnirvana@gmail.com>
2020-09-21 16:46:36 +0700

    move atta footnotes down

--- dev/common-corrections.js
+++ dev/common-corrections.js
@@ -10,8 +10,48 @@ const sourceDir = path.join(__dirname, '../public/static/text')
 const outputDir = path.join(sourceDir, 'corrected')
 let processedFilesCount = 0
 
-const fileFilter = /./g
+const fileFilter = /sn/g
 const operation = 'format'
+
+const operationsList = {
+    format: (data) => {
+        let modCount = 0
+        data.pages.forEach(p => p.sinh.entries.forEach(e => {
+            if ('format' in e) {
+                delete e.format
+                modCount++
+            }
+        }))
+        return modCount
+    },
+
+    attaFootnotes: (data) => {
+        let modCount = 0
+        data.pages.forEach(p => {
+            const footnotes = [], dedupFootnotes = {}
+            p.sinh.entries.forEach(e => extractFootnotes(e, footnotes, dedupFootnotes))
+            if (footnotes.length) {
+                if (p.sinh.footnotes.length) console.error(`page ${p.pageNum} already has footnotes`)
+                p.sinh.footnotes = footnotes
+                modCount += footnotes.length
+                //console.log(footnotes)
+            }
+        })
+        return modCount
+    }
+}
+
+function extractFootnotes(entry, footnotes, dedupFootnotes) {
+    entry.text = entry.text.replace(/\{[\. ]*([^\{\}]{3,})\}/g, (match, p1) => { // only >= 3 letters long
+        p1 = p1.trim()
+        const ref = dedupFootnotes[p1] || footnotes.length + 1
+        if (!dedupFootnotes[p1]) footnotes.push({ type: 'footnote', text: ref + '. ' + p1 })
+        dedupFootnotes[p1] = ref
+        return `{${ref}}`
+    })
+}
+
+
 if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir)
 const filteredFiles = fs.readdirSync(sourceDir).filter(file => path.extname(file) == '.json' && fileFilter.test(file))
 console.log(`selected ${filteredFiles.count} with filter ${fileFilter} and operation ${operation}`)
@@ -21,21 +61,10 @@ console.log(`processed ${processedFilesCount} files and wrote to ${outputDir}`)
 
 function processFile(fullName) {
     const data = JSON.parse(fs.readFileSync(path.join(sourceDir, fullName), 'utf-8'))
-    const modCount = format(data)
+    const modCount = operationsList[operation](data)
     if (modCount > 0) {
         console.log(`processed file ${fullName} with ${modCount} changes with ${operation}`)
         fs.writeFileSync(path.join(outputDir, fullName), vkb.json(JSON.stringify(data)), 'utf-8')
         processedFilesCount++
     }
-}
-
-function format(data) {
-    let modCount = 0
-    data.pages.forEach(p => p.sinh.entries.forEach(e => {
-        if ('format' in e) {
-            delete e.format
-            modCount++
-        }
-    }))
-    return modCount
 }
