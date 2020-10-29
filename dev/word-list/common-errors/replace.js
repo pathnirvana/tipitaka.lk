b0028c077440fe74d30739933f6e03adc10fde84
pathnirvana <pathnirvana@gmail.com>
2020-10-29 23:00:54 +0700

    1-common-errors replacements
    1-done-replacements has the count of each replacement done
    
    == output from replace.js ==
    wrote new ignore list with 490 words to pali-ignore-new.json
    needs to do 1542 replacements
    changed 167 files out of 177 files, in 3343.866600036621 mills
    ඉරියති freq 2, but found 1 places to replace
    උපවිතත්තා freq 3, but found 1 places to replace
    තීභි freq 1, but found 0 places to replace
    බහූතරා freq 1, but found 0 places to replace
    බහූන්නං freq 2, but found 1 places to replace

---
+++ dev/word-list/common-errors/replace.js
@@ -0,0 +1,62 @@
+/**
+ * read error checked files for common errors and make the corrections
+ * empty: carry forward to next list - do nothing
+ * c/m/d: create a list of correct words that will not be included in the subsequent lists
+ * x: replace errorword with mainword
+ */
+"use strict"
+
+const fs = require('fs')
+const path = require('path')
+const vkb = require('vkbeautify'), perf = require('perf_hooks').performance
+const { readWordList, processTextFiles } = require('../common-functions.js')
+const checkedFilename = 'pali-checked-1.txt'
+const ignoreFilename = 'pali-ignore.json', newIgnoreFilename = 'pali-ignore-new.json'
+
+const ignoreWords = JSON.parse(fs.readFileSync(path.join(__dirname, ignoreFilename), 'utf-8')), replacements = {}
+const input = fs.readFileSync(path.join(__dirname, checkedFilename), 'utf-8').split('\n').forEach(line => {
+    const cells = line.split('\t').filter(c => c.trim()).map(cell => cell.match(/([\u0D80-\u0DFF]+)\/(\d+)([mcdx]?)/))
+    //console.log(cells)
+    if (cells.some(m => !m)) console.error(`malformed line in ${checkedFilename} ${line}`)
+    const mainWord = cells[0][1]
+    cells.slice(1).forEach(([_1, word, freq, action]) => {
+        if (['c', 'm', 'd'].indexOf(action) >= 0) ignoreWords[word] = action
+        else if (['x'].indexOf(action) >= 0) {
+            if (replacements[word] && replacements[word].mainWord != mainWord) 
+                console.error(`replacement for this word ${word} already defined -> ${replacements[word].mainWord}/${mainWord}`)
+            replacements[word] = { mainWord, freq, done: 0 }
+        }
+    })    
+})
+fs.writeFileSync(path.join(__dirname, newIgnoreFilename), vkb.json(JSON.stringify(ignoreWords)), 'utf-8')
+console.log(`wrote new ignore list with ${Object.keys(ignoreWords).length} words to ${newIgnoreFilename}`)
+
+console.log(`needs to do ${Object.keys(replacements).length} replacements`)
+
+function makeReplacements(data) {
+    let modCount = 0
+    const replaceFunc = (e) => {
+        e.text = e.text.replace(/[\u0D80-\u0DFF]+/g, (m) => {
+            const info = replacements[m]
+            if (info) {
+                info.done++
+                modCount++
+                return info.mainWord
+            }
+            return m
+        })
+    }
+    data.pages.forEach(p => {
+        p.pali.entries.forEach(e => replaceFunc(e))
+        //p.sinh.entries.forEach(e => replaceFunc(e))
+    })
+    return modCount
+}
+
+const perf1 = perf.now()
+const modCounts = processTextFiles(file => !/^atta/.test(file), (data, file) => makeReplacements(data), false)
+const considered = Object.keys(modCounts).length, changed = Object.values(modCounts).filter(v => v).length
+console.log(`changed ${changed} files out of ${considered} files, in ${perf.now() - perf1} mills`)
+
+Object.entries(replacements).filter(([w, info]) => info.done != info.freq).forEach(([w, info]) => console.log(`${w} freq ${info.freq}, but found ${info.done} places to replace`))
+fs.writeFileSync(path.join(__dirname, 'done-replacements.json'), vkb.json(JSON.stringify(replacements)), 'utf-8')
