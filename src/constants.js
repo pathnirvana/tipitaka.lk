74ca7c828af9a0f109fb111e4f284ca26ffb8df5
pathnirvana <pathnirvana@gmail.com>
2020-11-06 20:59:16 +0700

    adding 3-common-errors

--- src/constants.js
+++ src/constants.js
@@ -9,9 +9,9 @@ const searchSettingsVersion = '1'
 export const searchSettingsKey = `tipitaka.lk-search-settings-${searchSettingsVersion}`
 
 export const filterTreeParents = ['vp','sp','ap','atta-sp','dn','mn','sn','an','kn']
-export const allFilterKeys = ["vp-prj","vp-pct","vp-mv","dn-1","dn-2","dn-3","mn-1","mn-2","mn-3","sn-1","sn-2","sn-3","sn-4","sn-5",
+export const allFilterKeys = ["vp-prj","vp-pct","vp-mv","vp-cv","vp-pv","dn-1","dn-2","dn-3","mn-1","mn-2","mn-3","sn-1","sn-2","sn-3","sn-4","sn-5",
 "an-1","an-2","an-3","an-4","an-5","an-6","an-7","an-8","an-9","an-10","an-11","kn-khp","kn-dhp","kn-ud","kn-iti","kn-snp",
-"kn-vv","kn-pv","kn-thag","kn-thig","kn-jat","kn-ps","kn-ap","kn-bv","kn-cp","kn-nett","kn-petk","ap-dhs","ap-vbh","ap-kvu",
+"kn-vv","kn-pv","kn-thag","kn-thig","kn-mn","kn-nc","kn-jat","kn-ps","kn-ap","kn-bv","kn-cp","kn-nett","kn-petk","ap-dhs","ap-vbh","ap-kvu",
 "ap-dhk","ap-pug","ap-yam","ap-pat","atta-dn","atta-mn","atta-sn","atta-an","atta-kn"]
 
 export const allFilterLength = allFilterKeys.length
