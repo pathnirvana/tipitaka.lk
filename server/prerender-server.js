3de79f1bb197ea9500f99120ee510ffd6ea261cc
pathnirvana <pathnirvana@gmail.com>
2021-10-27 22:52:09 +0700

    adding atta-kn-thig and atta-kn-thag

--- server/prerender-server.js
+++ server/prerender-server.js
@@ -5,17 +5,18 @@
  * make a request to the server such as 
  * curl http://localhost:3000/render?url=https://tipitaka.lk/mn/sinh
  * 
- * middleware prerender-node has to be setup on the fastify server to get the rendered page from prerender
- * when the request is from a webserver
  * 
  * prod run as follows (ubuntu)
- * pm2 start prerender-server.js
+ * make sure to npm install the prerender module first
+ * pm2 start server/prerender-server.js
  * pm2 save (save after changing any process parameters)
  */
 
 const prerender = require('prerender')
 const server = prerender({
     // this will work only in ubuntu - comment out if running in windows
-    chromeLocation: '/usr/bin/chromium-browser',
+    chromeLocation: '/usr/bin/google-chrome-stable',
+    chromeFlags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
+    pageLoadTimeout: 15 * 1000,
 })
 server.start()
