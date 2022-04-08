664702f2b1d3e59c1d5100d088df514502d1098a
pathnirvana <pathnirvana@gmail.com>
2022-04-08 14:25:57 +0700

    adding atta-kn-ap balanced

--- dev/build-tree.js
+++ dev/build-tree.js
@@ -68,8 +68,8 @@ const tree = {
     'atta-kn': [ 'ඛුද්දකනිකායො අට්ඨකථා', 'කුදුගත් සඟිය අටුවාව',      6, [0, 0], 'atta-sp', 'atta-kn-khp'],
     
     'atta-kn-khp': [], 'atta-kn-dhp': [], 'atta-kn-ud':  [], 'atta-kn-iti': [], 'atta-kn-snp': [], 'atta-kn-vv': [], 'atta-kn-pv': [], // order
-    'atta-kn-thag': [], 'atta-kn-thig': [], 'atta-kn-jat': [], 'atta-kn-mn': [], 'atta-kn-nc': [], 'atta-kn-ps': [], 'atta-kn-bv': [], //order
-    'atta-kn-cp': [], // order
+    'atta-kn-thag': [], 'atta-kn-thig': [], 'atta-kn-jat': [], 'atta-kn-mn': [], 'atta-kn-nc': [], 'atta-kn-ps': [], 'atta-kn-ap': [], // order
+    'atta-kn-bv': [], 'atta-kn-cp': [], // 'atta-kn-nett': [] order
 
     'atta-ap-dhs': [], // todo: atta link probably can be improved with more knowledge
     'atta-ap-vbh': [], // due to headingsAtEnd and multilevel in atta, attalink was very hacky. TODO- add headings at beginning to mula
