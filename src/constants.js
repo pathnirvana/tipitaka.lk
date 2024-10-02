e09925ecbdac5bb7d6d51ddee577908e286dee75
pathnirvana <pathnirvana@gmail.com>
2024-10-02 18:27:50 +0700

    fix few errors in headings and add new anya key to the tree

--- src/constants.js
+++ src/constants.js
@@ -12,7 +12,7 @@ export const filterTreeParents = ['vp','sp','ap','atta-sp','dn','mn','sn','an','
 export const allFilterKeys = ["vp-prj","vp-pct","vp-mv","vp-cv","vp-pv","dn-1","dn-2","dn-3","mn-1","mn-2","mn-3","sn-1","sn-2","sn-3","sn-4","sn-5",
 "an-1","an-2","an-3","an-4","an-5","an-6","an-7","an-8","an-9","an-10","an-11","kn-khp","kn-dhp","kn-ud","kn-iti","kn-snp",
 "kn-vv","kn-pv","kn-thag","kn-thig","kn-mn","kn-nc","kn-jat","kn-ps","kn-ap","kn-bv","kn-cp","kn-nett","kn-petk","ap-dhs","ap-vbh","ap-kvu",
-"ap-dhk","ap-pug","ap-yam","ap-pat","atta-vp","atta-dn","atta-mn","atta-sn","atta-an","atta-kn","atta-ap","anya-vm"]
+"ap-dhk","ap-pug","ap-yam","ap-pat","atta-vp","atta-dn","atta-mn","atta-sn","atta-an","atta-kn","atta-ap","anya"]
 
 export const allFilterLength = allFilterKeys.length
 
